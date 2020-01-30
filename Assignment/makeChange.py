06f90612810fb52a51b457e83d4853c0bd03207e
harbint <travisharbin@gmail.com>
2020-01-30 14:56:04 -0600

    first commit

--- Assignment/makeChange.py
+++ Assignment/makeChange.py
@@ -1,2 +1,45 @@
-def makeChange():     
-    pass
+from _pydecimal import Decimal
+def makeChange(amount)  :
+    if isinstance(amount, (int,float)):
+        amount = Decimal(amount)
+        if amount >= 0 and amount <= 100:
+            solution = [0,0,0,0,0,0,0,0]
+            amount = round(amount,2)
+            while amount > 0:
+                if amount >= 20:
+                    solution[0] += 1
+                    amount -= Decimal(20)
+                    continue
+                elif amount >= 10:
+                    solution[1] += 1
+                    amount -= Decimal(10)
+                    continue
+                elif amount >= 5:
+                    solution[2] += 1
+                    amount -= Decimal(5)
+                    continue
+                elif amount >= 1:
+                    solution[3] += 1
+                    amount -= Decimal(1)
+                    continue
+                elif amount >= .25:
+                    solution[4] += 1
+                    amount -= Decimal(.25)
+                    continue
+                elif amount >= .10:
+                    solution[5] += 1
+                    amount -= Decimal(.10)
+                    continue
+                elif amount >= .05:
+                    solution[6] += 1
+                    amount -= Decimal(.05)
+                    continue
+                else:
+                    solution[7] += 1
+                    amount -= Decimal(.01)
+        else:
+            solution = []
+    else:
+        solution = []
+    return solution
+        
--- Assignment/makeChangeTest.py
+++ Assignment/makeChangeTest.py
@@ -10,4 +10,14 @@ class MakeChangeTest(unittest.TestCase):
         pass
 
     def testName(self):
-        pass
+        self.assertEqual([0,0,0,0,0,0,0,0], mc.makeChange(0))
+        
+        self.assertEqual([], mc.makeChange(-1)) #tests out of bounds lower
+        self.assertEqual([], mc.makeChange(100.1)) #tests out of bounds upper
+        
+        self.assertEqual([0,0,0,1,0,0,0,0], mc.makeChange(1))
+        
+        self.assertEqual([0,0,0,0,1,0,0,1], mc.makeChange(.256)) #tests rounding up to the nearest penny
+        #self.assertEqual([1,0,0,0,0,0,0,0], mc.makeChange(20.001)) #tests rounding down 
+        
+        self.assertEqual([], mc.makeChange("hello there")) #test to assure only accept int/float values
