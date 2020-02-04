b30548e05ff43c3c6c5b49350827b854c7d9e0f5
harbint <travisharbin@gmail.com>
2020-02-04 12:22:59 -0600

    final commit

--- Assignment/makeChange.py
+++ Assignment/makeChange.py
@@ -1,42 +1,46 @@
-from _pydecimal import Decimal
-def makeChange(amount)  :
-    if isinstance(amount, (int,float)):
-        amount = Decimal(amount)
-        if amount >= 0 and amount <= 100:
-            solution = [0,0,0,0,0,0,0,0]
-            amount = round(amount,2)
-            while amount > 0:
-                if amount >= 20:
-                    solution[0] += 1
-                    amount -= Decimal(20)
-                    continue
-                elif amount >= 10:
-                    solution[1] += 1
-                    amount -= Decimal(10)
-                    continue
-                elif amount >= 5:
-                    solution[2] += 1
-                    amount -= Decimal(5)
-                    continue
-                elif amount >= 1:
-                    solution[3] += 1
-                    amount -= Decimal(1)
-                    continue
-                elif amount >= .25:
-                    solution[4] += 1
-                    amount -= Decimal(.25)
-                    continue
-                elif amount >= .10:
-                    solution[5] += 1
-                    amount -= Decimal(.10)
-                    continue
-                elif amount >= .05:
-                    solution[6] += 1
-                    amount -= Decimal(.05)
-                    continue
-                else:
-                    solution[7] += 1
-                    amount -= Decimal(.01)
+from _decimal import ROUND_HALF_UP
+from _decimal import Decimal
+def makeChange(amount=None): #function definition with optional variable to handle nothing input
+    if amount!= None:
+        if isinstance(amount, (int,float)): #check to make sure that input is a int or float
+            amount = Decimal(str(amount)) #convert float or int to string and create decimal object to avoid rounding error
+            if amount >= 0 and amount <= 100: #boundary conditions 0-100
+                solution = [0,0,0,0,0,0,0,0]
+                amount = Decimal(amount).quantize(Decimal('000.00'),ROUND_HALF_UP) #handling python3's desire to round like a banker
+                while amount > 0:
+                    if amount >= 20:
+                        solution[0] += 1
+                        amount -= Decimal(20)
+                        continue
+                    elif amount >= 10:
+                        solution[1] += 1
+                        amount -= Decimal(10)
+                        continue
+                    elif amount >= 5:
+                        solution[2] += 1
+                        amount -= Decimal(5)
+                        continue
+                    elif amount >= 1:
+                        solution[3] += 1
+                        amount -= Decimal(1)
+                        continue
+                    elif amount >= .25:
+                        solution[4] += 1
+                        amount -= Decimal(.25)
+                        continue
+                    elif amount >= .10:
+                        solution[5] += 1
+                        amount -= Decimal(.10)
+                        continue
+                    elif amount >= .05:
+                        solution[6] += 1
+                        amount -= Decimal(.05)
+                        continue
+                    else:
+                        solution[7] += 1
+                        amount -= Decimal(.01)
+            else:
+                solution = []
         else:
             solution = []
     else:
--- Assignment/makeChangeTest.py
+++ Assignment/makeChangeTest.py
@@ -10,14 +10,20 @@ class MakeChangeTest(unittest.TestCase):
         pass
 
     def testName(self):
-        self.assertEqual([0,0,0,0,0,0,0,0], mc.makeChange(0))
-        
+        #data bounds tests
         self.assertEqual([], mc.makeChange(-1)) #tests out of bounds lower
         self.assertEqual([], mc.makeChange(100.1)) #tests out of bounds upper
-        
-        self.assertEqual([0,0,0,1,0,0,0,0], mc.makeChange(1))
-        
-        self.assertEqual([0,0,0,0,1,0,0,1], mc.makeChange(.256)) #tests rounding up to the nearest penny
-        #self.assertEqual([1,0,0,0,0,0,0,0], mc.makeChange(20.001)) #tests rounding down 
-        
         self.assertEqual([], mc.makeChange("hello there")) #test to assure only accept int/float values
+        self.assertEqual([], mc.makeChange()) #tests input of nothing
+        
+        #general use tests
+        self.assertEqual([0,0,0,0,0,0,0,0], mc.makeChange(0))
+        self.assertEqual([0,0,0,1,0,0,0,0], mc.makeChange(1))
+        self.assertEqual([0,0,0,3,1,0,0,1], mc.makeChange(3.26))
+        self.assertEqual([0,0,1,0,0,0,0,0], mc.makeChange(5))
+        self.assertEqual([1,1,1,1,1,1,1,1], mc.makeChange(36.41))
+        self.assertEqual([0,0,0,1,0,0,0,0], mc.makeChange(amount=1))
+        
+        #rounding tests
+        self.assertEqual([0,0,0,1,0,0,0,0], mc.makeChange(1.001)) #tests rounding down 
+        self.assertEqual([0,0,0,1,0,0,0,1], mc.makeChange(1.005)) #tests rounding up to the nearest penny
